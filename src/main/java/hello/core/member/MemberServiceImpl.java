4be835c098d97eb41e3f81012cb9c1e18de27c6b
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-03 19:08:07 +0900

    할인 정책 인터페이스 생성

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,6 +4,7 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
