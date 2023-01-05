1c75619c8aeb5aaa47b0a62923459f8c0f6ddfe4
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-05 21:27:36 +0900

    롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,21 +6,19 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
+    //롬복 설치시 final 필드를 자동 생성자처리해준다.
+    //생성자가 1개일시 자동 Autowired되므로, 코드가 간결해진다
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
