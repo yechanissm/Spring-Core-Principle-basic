061c491051a536e3851729bca3bb4a20a25c49c5
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-05 20:44:43 +0900

    스프링 빈 없이 동작할 때 - 옵션처리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
