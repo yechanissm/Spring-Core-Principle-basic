4dad90922ae85d70f48de4123d6411863ce008ed
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-05 13:56:35 +0900

    스프링 없는 순수한 DI 컨테이너

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //!=
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+        /*
+        우리가 만들었던 스프링 없는 순수한 DI 컨테이너인 AppConfig는 요청을 할 때마다
+        객체를 새로 생성한다.
+        고객 트래픽이 초당 100이 나오면 초당 100개 객체가 생성,소멸한다 -> 메모리 낭비
+        해결방은은 해당 객체가 1개만 생성되고 공유하도록 설계 - 싱글톤 패턴
+         */
+    }
+}
