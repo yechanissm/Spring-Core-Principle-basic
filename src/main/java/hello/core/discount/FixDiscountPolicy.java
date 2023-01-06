5a2d6dac282398245ccd62bc642e9fc0d9da10f8
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-01-06 17:50:49 +0900

    빈이 2개 이상 - 문제

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component //의존관계 자동 주입이 2개가 되므로, 오류가 발생
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
